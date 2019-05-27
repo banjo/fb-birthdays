b7008ece765bab1e40f36f67786bea5c5ba9451e
Anton Ödman <anton.odman@gmail.com>
2019-05-27 23:16:49 +0200

    added google calendar

--- google_calendar.py
+++ google_calendar.py
@@ -2,13 +2,12 @@ from __future__ import print_function
 import datetime
 import pickle
 import os.path
-import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def main():
@@ -36,19 +35,10 @@ def main():
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    calendar = {
+        'summary': 'Birthdays'
+    }
+    created_calendar = service.calendars().insert(body=calendar).execute()
 
 
 if __name__ == '__main__':
