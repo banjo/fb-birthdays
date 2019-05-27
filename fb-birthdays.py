acfffad51ed22aff6e89f04740f345ed8a118cd3
Anton Ödman <anton.odman@gmail.com>
2019-05-27 23:01:51 +0200

    update

--- fb-birthdays.py
+++ fb-birthdays.py
@@ -6,12 +6,34 @@ url = "https://www.facebook.com/events/ical/birthdays/?uid=1386784203&key=AQCzvW
 def main():
     data = get_data()
     data_list = data_to_list(data)
-    sorted_list = sort_list(data_list)
+    data_dict = get_dict_from_list(data_list)
+    # TODO: Add option to select which ones to add
+
+    # TODO: Add to calendar
 
 
-def sort_list(data_list):
+def get_dict_from_list(data_list):
+    dictionary = {}  # init dictionary
+
     for text in data_list:
-        input(text)
+        # Birthdate
+        birthday = text[10:18]
+
+        # Name
+        splitted = text.split("\n")
+        long_name = splitted[2]
+        name = long_name.replace("SUMMARY:", "")
+        name = name.replace(" fyller år", "")
+        name = name.replace("\r", "")
+
+        # create tuple
+        person_data = (name, birthday)
+
+        # put into dictionary
+        dictionary[name] = person_data
+
+    input(len(dictionary))
+    return dictionary
 
 
 def data_to_list(data):
